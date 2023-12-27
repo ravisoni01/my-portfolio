84a5f0592dcb4d4cdeadc2afc948ba82c79009a8
Ravi Soni <r7679242@gmail.com>
2023-12-27 21:11:32 +0530

    added loading animation

---
+++ src/components/page-loading.tsx
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+import loading from "@/assets/animation/loading.json";
+import Lottie from "lottie-web";
+
+export const PageLoading = () => {
+  useEffect(() => {
+    const container: any = document.querySelector("#loading_container");
+    const anim = Lottie.loadAnimation({
+      container: container,
+      animationData: loading,
+      renderer: "svg",
+      loop: true,
+      autoplay: true,
+    });
+
+    return () => {
+      anim.destroy();
+    };
+  }, []);
+
+  return <div className="bg-[#202020] h-full" id="loading_container" />;
+};
